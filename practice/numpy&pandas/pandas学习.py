de1e4e97ededaa053babfe2c4169efb229a23195
lab <944015847qq.com>
2019-05-30 14:46:37 +0800

    update

--- practice/numpy&pandas/pandas学习.py
+++ practice/numpy&pandas/pandas学习.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 
 #a = pd.DataFrame(np.random.randn(6,4), columns=['A','B','C','D'])
-a=pd.DataFrame(np.random.randn(6,4))
+a=pd.DataFrame(np.random.randn(6,4),columns=['A','B','C','D'])
 print(a)
-print(a.loc[1,2])#loc就是pandas的索引
+
+#iloc根据位置索引，loc根据位置的名称索引
+print(a.iloc[1,2])#loc就是pandas的索引
